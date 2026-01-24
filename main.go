745b3635a69b296ba0dca320b3ec38c7190af2c9
Patria Gani <patria.gani@gmail.com>
2026-01-24 13:08:15 +0700

    add category

--- main.go
+++ main.go
@@ -15,10 +15,21 @@ type Product struct {
 	Stok  int    `json:"stok"`
 }
 
+type Categories struct {
+	ID          int    `json:"id"`
+	Nama        string `json:"nama"`
+	Description string `json:"description"`
+}
+
 var produk = []Product{
 	{ID: 1, Nama: "Indomie Goreng", Harga: 3500, Stok: 10},
 	{ID: 2, Nama: "Indomie Rebus", Harga: 3500, Stok: 9},
 	{ID: 3, Nama: "Nasi Goreng", Harga: 5000, Stok: 8},
+}
+
+var kategori = []Categories{
+	{ID: 1, Nama: "Makanan", Description: "Semua jenis makanan"},
+	{ID: 2, Nama: "Minuman", Description: "Semua jenis minuman"},
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
@@ -102,6 +113,88 @@ func deleteProduct(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Product ID Not Found", http.StatusNotFound)
 }
 
+// POST /api/categories
+func createCategory(w http.ResponseWriter, r *http.Request) {
+	var kategoriBaru Categories
+	err := json.NewDecoder(r.Body).Decode(&kategoriBaru)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	kategoriBaru.ID = len(kategori) + 1
+	kategori = append(kategori, kategoriBaru)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode((kategoriBaru))
+}
+
+// GET /api/categories/{id}
+func getCategory(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+	for _, k := range kategori {
+		if k.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(k)
+			return
+		}
+	}
+	http.Error(w, "Category ID Not Found", http.StatusNotFound)
+}
+
+// PUT /api/categories/{id}
+func editCategory(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+	var kategoriUpdate Categories
+	err = json.NewDecoder(r.Body).Decode(&kategoriUpdate)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	for i := range kategori {
+		if kategori[i].ID == id {
+			kategoriUpdate.ID = id
+			kategori[i] = kategoriUpdate
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(kategoriUpdate)
+			return
+		}
+	}
+	http.Error(w, "Category ID Not Found", http.StatusNotFound)
+}
+
+// DELETE /api/categories/{id}
+func deleteCategory(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+	for i, k := range kategori {
+		if k.ID == id {
+			kategori = append(kategori[:i], kategori[i+1:]...)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "success delete",
+			})
+			return
+		}
+	}
+	http.Error(w, "Category ID Not Found", http.StatusNotFound)
+}
+
 func main() {
 	//GET /api/produk/{id}
 	//PUT /api/produk/{id}
@@ -125,6 +218,32 @@ func main() {
 			json.NewEncoder(w).Encode(produk)
 		} else if r.Method == "POST" {
 			createProduct(w, r)
+
+		}
+	})
+
+	//GET /api/categories/{id}
+	//PUT /api/categories/{id}
+	//DELETE /api/categories/{id}
+	http.HandleFunc("/api/categories/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			getCategory(w, r)
+		} else if r.Method == "PUT" {
+			editCategory(w, r)
+		} else if r.Method == "DELETE" {
+			deleteCategory(w, r)
+		}
+
+	})
+
+	//GET /api/categories
+	//POST /api/categories
+	http.HandleFunc("/api/categories", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(kategori)
+		} else if r.Method == "POST" {
+			createCategory(w, r)
 
 		}
 	})
